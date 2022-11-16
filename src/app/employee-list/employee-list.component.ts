0c323711ac8acda31cb3b116dfb85e22ef118779
ADMIN <Bharath Alwala>
2022-11-16 09:04:40 +0530

    Mat Table Implemented

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,8 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { EmployeeService } from '../services/employee.service';
-import {} from '@angular/material'
+import { Component, OnInit, ViewChild } from '@angular/core';
+
 import { ActivatedRoute, Router, RouterEvent } from '@angular/router';
 import { AnyCnameRecord } from 'dns';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { Employee } from '../model/employee.model';
+import { EmployeeService } from '../services/employee.service';
+
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
@@ -10,13 +14,37 @@ import { AnyCnameRecord } from 'dns';
 })
 export class EmployeeListComponent implements OnInit {
 
-  constructor(public empservice:EmployeeService,private route: ActivatedRoute,private router:Router ) { }
 
-  ngOnInit(): void {
-
-    this.empservice.refreshList();
+  constructor(public empservice: EmployeeService, private route: ActivatedRoute, private router: Router) {
   }
-  addEmployee(){
+  displayedColumns: string[] = ['name', 'age', 'dateofbirth', 'address'];
+  dataSource: any = new MatTableDataSource<Employee>();
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  emp: Employee[] = [];
+
+
+
+  addEmployee() {
     this.router.navigate(['/Add']);
   }
+
+
+  ngOnInit(): void {
+    this.getEmployeeData();
+  }
+
+  getEmployeeData() {
+    this.empservice.getEmployeeList().subscribe(
+      res => {
+        this.emp = res as Employee[];
+        this.dataSource = new MatTableDataSource<Employee>(this.emp);      
+      },
+      err => { console.log(err) }
+    );
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
 }
--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http'
+import { HttpClient } from '@angular/common/http'
 import { Employee } from '../model/employee.model';
 import { Observable } from 'rxjs/internal/Observable';
 import { Console } from 'console';
+import { EmployeeListComponent } from '../employee-list/employee-list.component';
 
 
 @Injectable({
@@ -10,29 +11,31 @@ import { Console } from 'console';
 })
 export class EmployeeService {
 
-  constructor(private http:HttpClient) { }
- 
+  constructor(private http: HttpClient) { }
+
   emp: Employee[] = [];
   empPost: Employee[] = [];
-  readonly baseURL ='https://localhost:7221/api/employee/employeelist'
+  readonly baseURL = 'https://localhost:7221/api/employee/employeelist'
   //formData:Employee = new Employee();
-  refreshList(){
-    this.http.get(this.baseURL)
-    .toPromise()
-    .then( 
-      res => this.emp = res as Employee[]    
-      );      
-      
- }  
+  refreshList() {
+    return this.http
+      .get(this.baseURL)
+      .subscribe({
+        next: (response) => this.emp = response as Employee[],
+        error: (error) => console.log("testlog" + error),
+      });
 
- saveEmployee(empPost:any){
-  debugger;
-  this.http
-  .post('https://localhost:7221/api/employee/PostEmployee', empPost)
-  .subscribe({
-    next: (response) => console.log(response),
-    error: (error) => console.log(error),
-  });
+  }
+  getEmployeeList(){
+  return this.http.get(this.baseURL);
+  }
+  saveEmployee(empPost: any) {
+    this.http
+      .post('https://localhost:7221/api/employee/PostEmployee', empPost)
+      .subscribe({
+        next: (response) => console.log(response),
+        error: (error) => console.log(error),
+      });
 
- }
+  }
 }
